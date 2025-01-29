b94d26385803ee8614ff5b85273065e5c07eebf3
stevenbenalcazar <sabenalcazarm@uce.edu.ec>
2025-01-29 16:50:37 -0500

    refactore

--- services/board/src/routes/boardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createBoard } = require('../controllers/boardController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/', authMiddleware, createBoard);
-
-module.exports = router;
--- services/workspace/src/routes/workspaceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createWorkspace } = require('../controllers/workspaceController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.post('/', authMiddleware, createWorkspace);
-
-module.exports = router;
