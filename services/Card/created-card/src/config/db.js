8c7d081a224903ce889993513f685d2328c2fde1
CinthiaDayanara <137329939+CinthiaDayanara@users.noreply.github.com>
2025-01-31 16:38:44 -0500

    feat: :fire: microservices

--- services/Card/created-card/src/config/db.js
+++
@@ -1,7 +0,0 @@
-import AWS from 'aws-sdk';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: process.env.AWS_REGION });
-export default dynamoDB;
